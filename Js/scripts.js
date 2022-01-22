1d9a50871c0bc4cfdbae5ef1f73cbd029ff23af4
sannyralph <66128393+sannyralph@users.noreply.github.com>
2022-01-22 18:11:29 +0100

    updated js
    pokemonList added

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,4 +1,5 @@
-alert('Pretty Cool!!!');
-
-let favFood = 'Pasta'
-document.write(favFood)
+let pokemonList = [
+                  {name: Metapod, height: 0.7, type:'stone'}
+                  {name: Pikachu, height: 0.3, type:['grras','stone']}
+                  {name: Persian, height: 0.6, type:['grras','stone','poison']}
+                ]
