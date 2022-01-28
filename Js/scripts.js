c28ea93e297be6747d9a8912dd75826377d1d3b5
sannyralph <66128393+sannyralph@users.noreply.github.com>
2022-01-29 00:23:48 +0100

    Updated code
    styles has been added and js code is more readable now

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,32 +1,50 @@
-let pokemonList = [
-                  {
-                  name:  'Metapod',
-                  height: 0.7,
-                  type:'stone'
-                  },
+let pokemonRepository = (function (){
+  let pokemonList = [
+                    {
+                    name:  'Metapod',
+                    height: 0.7,
+                    type:'stone'
+                    },
 
-                  {
-                  name:  'Pikachu',
-                  height: 0.3,
-                  type:['grass','stone']
-                  },
+                    {
+                    name:  'Pikachu',
+                    height: 0.3,
+                    type:['grass','stone']
+                    },
 
-                  {
-                  name:  'Persian',
-                  height: 0.5,
-                  type:['grass','stone','poison']
-                  }
-                ]
-  //the code below shows the names of each pokemon and adds the height beside it.
-  for (let i = 0; i < pokemonList.length; i++)
-  document.write( pokemonList[i].name + (', height:') + pokemonList[i].height + '<br>');
+                    {
+                    name:  'Persian',
+                    height: 0.5,
+                    type:['grass','stone','poison']
+                    }
+                  ];
+
+      function getAll(){
+        return pokemonList;
+      }
+
+      function add(item) {
+        pokemonRepository.add(item) }
+
+    return {
+        getAll : getAll,
+        add: add
+    }
+
+
+    })()
+
+    pokemonRepository.getAll().forEach(function(pokemon){
+      document.write("<p>" + pokemon.name + (',  height: ') + pokemon.height + "</p>" )
+    });
+
 
 // the code here adds a conditional that picks out any pokemon the has a hength that is greater than 0.6 and also adds a special message beside it.
-  for (let i = 0; i < pokemonList.length; i++)
-      if (pokemonList[i].height > 0.6) {
-    document.write('<br>' +  pokemonList[i].name + ( ', height: ' )+ pokemonList[i].height + ( " (Wow, That\'s big !!)") + '<br>');
- }
-// here I want the code to just return the rest of the pokemon that doesn't meet the initial requirment i.e greater 0.6 with adding any messaage 
- else {
-   document.write('<br>' +  pokemonList[i].name + ( ', height: ' )+ pokemonList[i].height + '<br>')
-  }
+  pokemonRepository.getAll().forEach(function(pokemon){
+    if (pokemon.height > 0.6)
+    document.write('<p>' +  pokemon.name + ( ', height: ' )+ pokemon.height + ( " (Wow, That\'s big !!)") + '</p>');
+
+    else {
+      document.write('<p>' +  pokemon.name + ( ', height: ' )+ pokemon.height + '</>')
+     }
+    })
