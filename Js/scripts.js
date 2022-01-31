f22bad04313bd03b9c00af6ca0a7e627d97794d3
sannyralph <66128393+sannyralph@users.noreply.github.com>
2022-01-31 11:30:43 +0100

    js & css update

--- Js/scripts.js
+++ Js/scripts.js
@@ -24,27 +24,44 @@ let pokemonRepository = (function (){
       }
 
       function add(item) {
-        pokemonRepository.add(item) }
+        pokemonRepository.add(item)
+      }
+// the code below will create  button list for the pokemonList we have
+      function addListItem(pokemon){
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listOfPokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add('button-class');
+        listOfPokemon.appendChild(button);
+        pokemonList.appendChild(listOfPokemon);
+
+// the code below create an eventlistener to the button created
+        buttonEventListener(button, pokemon);
+      }
+
+// the below function fires the button to react when it is "clicked"
+      function buttonEventListener(button, pokemon) {
+        button.addEventListener('click', function(){
+            showDetails(pokemon);
+        })
+
+      }
+// the code below calls the eventlistener function on the console.log when the is clicked 
+    function showDetails(pokemon){
+      console.log(pokemon);
+    }
 
     return {
         getAll : getAll,
-        add: add
+        add: add,
+        addListItem: addListItem
     }
 
 
-    })()
+  })();
 
     pokemonRepository.getAll().forEach(function(pokemon){
-      document.write("<p>" + pokemon.name + (',  height: ') + pokemon.height + "</p>" )
+      pokemonRepository.addListItem(pokemon);
+
     });
-
-
-// the code here adds a conditional that picks out any pokemon the has a hength that is greater than 0.6 and also adds a special message beside it.
-  pokemonRepository.getAll().forEach(function(pokemon){
-    if (pokemon.height > 0.6)
-    document.write('<p>' +  pokemon.name + ( ', height: ' )+ pokemon.height + ( " (Wow, That\'s big !!)") + '</p>');
-
-    else {
-      document.write('<p>' +  pokemon.name + ( ', height: ' )+ pokemon.height + '</>')
-     }
-    })
